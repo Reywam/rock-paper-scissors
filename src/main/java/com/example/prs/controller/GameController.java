fcff80ac3d25cb6187b13d0ea7e23c6b11c560bf
reywam <reywam@mail.ru>
2024-09-16 18:46:30 +0700

    Add gameId parameter to endpoints

--- src/main/java/com/example/prs/controller/GameController.java
+++ src/main/java/com/example/prs/controller/GameController.java
@@ -1,23 +1,29 @@
 package com.example.prs.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/game")
+@Slf4j
 public class GameController {
     @PostMapping("/start")
-    public ResponseEntity<?> startGame() {
+    public ResponseEntity<UUID> startGame() {
+        return ResponseEntity.ok().body(UUID.randomUUID());
+    }
+
+    @PatchMapping("/{gameId}/move")
+    public ResponseEntity<?> makeMove(@PathVariable UUID gameId) {
+        log.info("Make move in game with id: {}", gameId);
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping("/move")
-    public ResponseEntity<?> makeMove() {
-        return ResponseEntity.ok().build();
-    }
-
-    @PutMapping("/terminate")
-    public ResponseEntity<?> terminate() {
+    @PutMapping("/{gameId}/terminate")
+    public ResponseEntity<?> terminate(@PathVariable UUID gameId) {
+        log.info("Terminating game with id: {}", gameId);
         return ResponseEntity.ok().build();
     }
 
