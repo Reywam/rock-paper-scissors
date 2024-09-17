46c9390bdd0818b2a9735c8a909a0b2161bde7d6
reywam <reywam@mail.ru>
2024-09-17 12:07:13 +0700

    Add client-readable error description

---
+++ src/main/java/com/example/prs/exceptions/ErrorInfo.java
@@ -0,0 +1,12 @@
+package com.example.prs.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorInfo {
+    String description;
+}
--- src/main/java/com/example/prs/exceptions/ExceptionsHandler.java
+++ src/main/java/com/example/prs/exceptions/ExceptionsHandler.java
@@ -2,8 +2,10 @@ package com.example.prs.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
@@ -12,13 +14,13 @@ public class ExceptionsHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(GameTerminatedException.class)
-    public void handleGameTerminatedException(GameTerminatedException ex) {
-        log.info("Can't play in terminated game");
+    public @ResponseBody ErrorInfo handleGameTerminatedException(GameTerminatedException ex) {
+        return new ErrorInfo("Can't play in terminated game");
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(GameNotFoundException.class)
-    public void handleGameTerminatedException(GameNotFoundException ex) {
-        log.info("Game is not found");
+    public @ResponseBody ErrorInfo handleGameTerminatedException(GameNotFoundException ex) {
+        return new ErrorInfo("Game is not found");
     }
 }
